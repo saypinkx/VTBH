2ec49a7ea231f56759f91977f13a75bf4f7933d0
sixzerx <paprut538@gmail.com>
2023-10-06 20:13:54 +0300

    edit model User

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,16 @@
 from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Boolean, Table, Column
+from sqlalchemy.orm import mapped_column
 from datetime import datetime
 from database import Base
 
 
 class User(Base):
     __tablename__ = "User"
-    id = Column("id", Integer, primary_key=True, autoincrement=True)
-    email = Column("email", String, nullable=True)
-    username = Column("username", String, nullable=False)
-    password = Column("password", String, nullable=False)
-    registered_at = Column("registered_at", TIMESTAMP, default=datetime.utcnow)
-    is_superuser = Column("is_superuser", Boolean, default=False)
-    is_entity = Column("is_entity", Boolean, default=False)
-    is_active = Column("is_active", Boolean, default=False)
+    id = mapped_column("id", Integer, primary_key=True, autoincrement=True)
+    email = mapped_column("email", String, nullable=True)
+    username = mapped_column("username", String, nullable=False)
+    password = mapped_column("password", String, nullable=False)
+    registered_at = mapped_column("registered_at", TIMESTAMP, default=datetime.utcnow)
+    is_superuser = mapped_column("is_superuser", Boolean, default=False)
+    is_entity = mapped_column("is_entity", Boolean, default=False)
+    is_active = mapped_column("is_active", Boolean, default=False)
