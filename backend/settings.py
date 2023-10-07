b19b081043440769938aa3f94340a781bd7ff262
sixzerx <paprut538@gmail.com>
2023-10-07 12:03:01 +0300

    add cors

---
+++ backend/settings.py
@@ -0,0 +1,16 @@
+from main import app
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
