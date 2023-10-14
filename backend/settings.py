b9d4c7aef8004b4439e07fa4ea60d336395fe50f
sixzerx <paprut538@gmail.com>
2023-10-14 17:44:02 +0300

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -1,15 +1,20 @@
 from main import app
 from fastapi.middleware.cors import CORSMiddleware
 
-
 origins = [
     "http://localhost:8080",
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# origins = ['https://localhost:3000', 'https://localhost:8080']
+# app.add_middleware(CORSMiddleware, allow_origins=[origins], allow_credentials=True, allow_methods=["*"],
+#                    allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=['https://localhost:8080'], allow_credentials=True,
+                          allow_methods=["*"], allow_headers=["*"])
